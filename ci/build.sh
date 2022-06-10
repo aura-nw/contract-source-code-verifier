fa67a5a5aed95093cdbc933cb36568d1b5feae7f
doquockhanhan <doquockhanhan@gmail.com>
2022-06-10 15:31:44 +0700

    remove env var

--- ci/build.sh
+++ ci/build.sh
@@ -4,6 +4,5 @@ set -xe
 #Login to registry
 echo $GITHUB_PASSWORD | docker login ghcr.io -u $GITHUB_USERNAME --password-stdin
 #Build and push image
-export DOCKER_HOST="tcp://localhost:2375"
 docker build -t ${CONTAINER_RELEASE_IMAGE} -f Dockerfile .
 docker push ${CONTAINER_RELEASE_IMAGE}
