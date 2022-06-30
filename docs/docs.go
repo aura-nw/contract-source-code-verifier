84b3ed27a2baf28d7610fdd4310b9b8581651aa0
doquockhanhan <doquockhanhan@gmail.com>
2022-06-30 17:36:32 +0700

    update domain

--- docs/docs.go
+++ docs/docs.go
@@ -126,7 +126,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "verify.serenity.aurascan.io",
+	Host:             "verify-job.dev.aura.network",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Verify Smart Contract API",
