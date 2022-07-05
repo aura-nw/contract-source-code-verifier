0d31ed015da4af16bad0926c6af71679d8c1e449
doquockhanhan <doquockhanhan@gmail.com>
2022-07-05 17:12:07 +0700

    update swagger

--- docs/docs.go
+++ docs/docs.go
@@ -155,7 +155,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "verify-job.dev.aura.network",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Verify Smart Contract API",
