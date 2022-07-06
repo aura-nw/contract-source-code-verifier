3c43c0237bd9232a6e5c2984f7710874f8d7875e
doquockhanhan <doquockhanhan@gmail.com>
2022-07-06 08:57:13 +0700

    remove localhost

--- main.go
+++ main.go
@@ -43,5 +43,5 @@ func main() {
 
 	url := ginSwagger.URL("http://localhost:8080/swagger/doc.json") // The url pointing to API definition
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler, url))
-	router.Run("localhost:8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
